08dc4f6fdf948e36e40ed0cc13e0f4837ef1d23e
Bruno Luiz <brunoslvrlz13@gmail.com>
2021-12-31 15:20:02 -0300

    Splash -  Refactoring Screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,13 +1,68 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import {
     Container
 } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
+import Animated, {
+    interpolate,
+    runOnJS,
+    useAnimatedStyle,
+    useSharedValue,
+    withTiming
+} from 'react-native-reanimated';
+
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
+
+interface NavigationProps {
+    navigate: (screen: string) => void;
+}
 
 export function Splash() {
+    const splashAnimated = useSharedValue(0);
+    const navigation = useNavigation<NavigationProps>();
+
+    const brandAnimation = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(splashAnimated.value,
+                [0, 10, 20],
+                [1, .3, 0]
+            )
+        }
+    })
+
+    const logoAnimation = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(splashAnimated.value,
+                [40, 50, 60, 65, 70],
+                [0, .3, 1, .3, 0]
+            )
+        }
+    })
+
+    function startApp() {
+        navigation.navigate('Home');
+    }
+
+    useEffect(() => {
+        splashAnimated.value = withTiming(70, { duration: 6000 },
+            () => {
+                'worklet'
+                runOnJS(startApp)();
+            }
+        )
+    }, [])
+
     return (
         <Container>
+            <Animated.View style={[brandAnimation, { position: 'absolute' }]}>
+                <BrandSvg width={80} height={50} />
+            </Animated.View>
 
+            <Animated.View style={[logoAnimation, { position: 'absolute' }]}>
+                <LogoSvg width={180} height={20} />
+            </Animated.View>
         </Container>
     );
 }
